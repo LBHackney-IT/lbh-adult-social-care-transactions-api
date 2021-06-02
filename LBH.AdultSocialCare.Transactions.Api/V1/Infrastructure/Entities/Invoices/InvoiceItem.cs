a457da2b2e7fa756499de43362660cc1c81bf95c
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-06-02 14:19:53 +0300

    Reference supplier return item in invoice items if it is the source of invoice

--- LBH.AdultSocialCare.Transactions.Api/V1/Infrastructure/Entities/Invoices/InvoiceItem.cs
+++ LBH.AdultSocialCare.Transactions.Api/V1/Infrastructure/Entities/Invoices/InvoiceItem.cs
@@ -6,30 +6,20 @@ namespace LBH.AdultSocialCare.Transactions.Api.V1.Infrastructure.Entities.Invoic
 {
     public class InvoiceItem : BaseEntity
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid InvoiceItemId { get; set; }
-
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] public Guid InvoiceItemId { get; set; }
         public Guid InvoiceId { get; set; }
-
+        public int InvoiceItemPaymentStatusId { get; set; }
         public string ItemName { get; set; }
-
         public decimal PricePerUnit { get; set; }
-
         public int Quantity { get; set; }
-
         public decimal SubTotal { get; set; }
-
-        public decimal Vat { get; set; }
-
+        public decimal VatAmount { get; set; }
         public decimal TotalPrice { get; set; }
-
-        public Guid LedgerId { get; set; }
-
-        [ForeignKey((nameof(InvoiceId)))]
-        public Invoice Invoice { get; set; }
-
+        public Guid? SupplierReturnItemId { get; set; } // If the invoice is coming from supplier returns, reference the item here
         public Guid CreatorId { get; set; }
-
         public Guid? UpdaterId { get; set; }
+
+        [ForeignKey((nameof(InvoiceId)))] public Invoice Invoice { get; set; }
+        [ForeignKey((nameof(InvoiceItemPaymentStatusId)))] public InvoiceItemPaymentStatus InvoiceItemPaymentStatus { get; set; }
     }
 }
