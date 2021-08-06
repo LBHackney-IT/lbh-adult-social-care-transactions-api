1655e01d8aa4563e37ed7186c25eebfe8281aea0
dunnudge <duncan.okeno@nudgedigital.co.uk>
2021-08-06 17:06:09 +0300

    Delete DB comment

--- LBH.AdultSocialCare.Transactions.Api/Startup.cs
+++ LBH.AdultSocialCare.Transactions.Api/Startup.cs
@@ -285,6 +285,9 @@ namespace LBH.AdultSocialCare.Transactions.Api
             {
                 DatabaseContext databaseContext = appScope.ServiceProvider.GetRequiredService<DatabaseContext>();
 
+                // Uncomment next line to delete and recreate DB
+                // databaseContext.Database.EnsureDeleted();
+
                 // Run pending database migrations
                 if (databaseContext.Database.GetPendingMigrations().Any())
                 {
