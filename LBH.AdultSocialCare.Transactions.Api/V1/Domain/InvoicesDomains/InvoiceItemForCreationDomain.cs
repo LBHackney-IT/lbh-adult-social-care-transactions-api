64b40fb34c2531981fcea2ffe67a6d73ec41f5d4
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-08-11 13:01:33 +0100

    Change invoice item quantity data type int to decimal

--- LBH.AdultSocialCare.Transactions.Api/V1/Domain/InvoicesDomains/InvoiceItemForCreationDomain.cs
+++ LBH.AdultSocialCare.Transactions.Api/V1/Domain/InvoicesDomains/InvoiceItemForCreationDomain.cs
@@ -6,7 +6,7 @@ namespace LBH.AdultSocialCare.Transactions.Api.V1.Domain.InvoicesDomains
     {
         public string ItemName { get; set; }
         public decimal PricePerUnit { get; set; }
-        public int Quantity { get; set; }
+        public decimal Quantity { get; set; }
         public decimal SubTotal { get; set; }
         public decimal VatAmount { get; set; }
         public decimal TotalPrice { get; set; }
---
+++ LBH.AdultSocialCare.Transactions.Api/V1/Infrastructure/Migrations/20210811115941_InvoiceItemUpdates.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LBH.AdultSocialCare.Transactions.Api.V1.Infrastructure.Migrations
+{
+    public partial class InvoiceItemUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Quantity",
+                table: "InvoiceItems",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "InvoiceItems",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
