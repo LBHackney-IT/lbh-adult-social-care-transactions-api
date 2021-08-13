91a2a33f3d218d72fe919e4c278d671a0bf38175
FurkanKayar55 <63727079+FurkanKayar55@users.noreply.github.com>
2021-08-13 14:05:38 +0100

    Precision on decimal fields

--- LBH.AdultSocialCare.Transactions.Api/Startup.cs
+++ LBH.AdultSocialCare.Transactions.Api/Startup.cs
@@ -311,8 +311,8 @@ namespace LBH.AdultSocialCare.Transactions.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DatabaseContext databaseContext)
         {
-            // Uncomment next line to delete and recreate DB
-            //databaseContext.Database.EnsureDeleted();
+            //Uncomment next line to delete and recreate DB
+            databaseContext.Database.EnsureDeleted();
 
             // Run pending database migrations
             if (databaseContext.Database.GetPendingMigrations().Any())
